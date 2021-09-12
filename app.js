d846957086660d6f156792f58d32e311c2790c31
Digambar <deshawaldigambar@gmail.com>
2021-09-12 23:36:42 +0530

    input output processing done
    completes input output processing done

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var billAmount = document.querySelector("#bill-amnt");
 var cashGiven = document.querySelector("#cash-given");
 var checkButton = document.querySelector("#check-btn");
 var errorMsg = document.querySelector("#error-msg");
+var noOfNotes= document.querySelectorAll(".no-of-notes");
+
+notes=["2000","500","100","50","10","1"];
 
 checkButton.addEventListener("click", function checkReturn(){
     if (billAmount.value>0){
         if(cashGiven.value>0){
             if(cashGiven.value>=billAmount.value){
-                let amountToBeReturn = cashGiven.value-billAmount.value;
+                var  amountToBeReturn = cashGiven.value-billAmount.value;
                 calculateChangeToBeGiven(amountToBeReturn);
                 }
                 
@@ -25,6 +28,13 @@ checkButton.addEventListener("click", function checkReturn(){
    }
 );
 
+function calculateChangeToBeGiven(amountToBeReturn){
+    for (var i=0; i<notes.length; i++){
+        var numberOfNotes= Math.trunc(amountToBeReturn/notes[i]);
+        amountToBeReturn%=notes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
 
 
 
