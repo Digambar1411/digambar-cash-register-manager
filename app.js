8342e012221ee87d2cfe367365184a35aaba0adc
Digambar <deshawaldigambar@gmail.com>
2021-09-12 22:43:27 +0530

    Input validation done
    completes the input accept and validation

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 var billAmount = document.querySelector("#bill-amnt");
-console.log(billAmount.value);
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-btn");
+var errorMsg = document.querySelector("#error-msg");
+
+checkButton.addEventListener("click", function checkReturn(){
+    if (billAmount.value>0){
+        if(cashGiven.value>0){
+            if(cashGiven.value>=billAmount.value){
+                let amountToBeReturn = cashGiven.value-billAmount.value;
+                calculateChangeToBeGiven(amountToBeReturn);
+                }
+                
+            else{
+                    errorMsg.innerText="Do you wanna wash a plates";
+                }
+        }
+        else{
+            errorMsg.innerText="Cash given should be greater than zero";
+        }
+    }
+    else{
+        errorMsg.innerText="Bill amount should be greater than zero";
+    }
+   }
+);
+
+
+
+
 
